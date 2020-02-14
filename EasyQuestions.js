2ddc01962f8bdcb562a3c600f479aad850ad0117
Jeremy Lleva <llevajeremy@yahoo.com>
2020-02-14 09:20:56 -0800

    First Commit

---
+++ EasyQuestions.js
@@ -0,0 +1,13 @@
+//Two Sum 
+//Given an array of ints return the indicies of the two numbers
+//Each input has only one solution
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+**/
+var twoSum = function(nums, target) {
+    if(nums.length === 2) 
+    	{return [0,1]}
+    
