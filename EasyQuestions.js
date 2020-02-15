9cd40336813e31d34e62e6f1d67ddfbf14fd2777
Jeremy Lleva <llevajeremy@yahoo.com>
2020-02-15 14:31:59 -0800

    Second commit

--- EasyQuestions.js
+++ EasyQuestions.js
@@ -10,4 +10,18 @@
 var twoSum = function(nums, target) {
     if(nums.length === 2) 
     	{return [0,1]}
-    
+    for(let i = 0; i < nums.length ; i++){
+    	for(let j = 0; j < nums.length; j++){
+    		if(nums[i] + nums[j] === target && i != j){
+    			return [i,j];
+    		}
+    	}
+    }
+}
+
+let twoSumsTest = [2,7,11,15];
+console.log("twoSum: ")
+console.log("Array: " + twoSumsTest +", Target: " + 9 + " = " + "[" + twoSum(twoSumsTest, 9) + "]");
+// console.log(twoSum(twoSumsTest, 9));
+//expected [0,1]
+
