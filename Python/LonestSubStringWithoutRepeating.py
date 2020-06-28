4490a6086b97d6f4738f9714853f5d9344f557c2
JeremyLleva <llevajeremy@gmail.com>
2020-06-27 19:20:41 -0700

    3. Lonest substring

---
+++ Python/LonestSubStringWithoutRepeating.py
@@ -0,0 +1,18 @@
+#Given a string, find the length of the longest substring without repeating characters.
+#Sliding door solution.
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        maxWindow = 0
+        start = end = 0
+        alphabets = set()
+        
+        while( end < len(s) ):
+            if( s[end] not in alphabets ):
+                alphabets.add(s[end])
+                end += 1
+                maxWindow = max( maxWindow, end - start )
+            else:
+                alphabets.remove( s[start] )
+                start += 1
+        return maxWindow
